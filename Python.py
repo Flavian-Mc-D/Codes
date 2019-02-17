0a65665895c3c529b26c93a8b77773eb2830359d
Flavian Mc Donald <flavian.mosquera@blueshift.com.br>
2019-02-17 12:40:23 -0300

    Arquivo Python modificado

--- Python.py
+++ Python.py
@@ -1,5 +1,8 @@
-# Comentarios de Python son iniciados con #
 #Python
+# Comentarios de Python son iniciados con #
+"""También hay comentarios de 
+varias lineas"""
+
 #Para ver información en pantalla:
 print('hola mundo')
 
@@ -7,11 +10,43 @@ print('hola mundo')
 
 mi_variable = 5
 print (mi_variable)
-
+#5
 #Azul en S.T. es el color de print, fusía el = y morado el 5, en pantalla se vé el 5 solamente.
+mi_variable_2 = """ 
+Cadena 
+de
+varias 
+lineas 
+"""
+print (mi_variable_2)
+#Cadena 
+#de
+#varias 
+#lineas
 
 #También existen las CONSTANTES que son valores fijos, no editables. representadas en mayusculas.
-
 MI_CONSTANTE = 'VALOR_NO_CAMBIANTE'
 print (MI_CONSTANTE)
+#VALOR_NO_CAMBIANTE
+
+#Número entero:
+edad_e = 35
+print (edad_e)
+#35
+#Número entero octal:
+edad_o = 0.43
+print (edad_o)
+#0.43
+#Número entero hexadecimal:
+edad_h = 0.10 
+print (edad_h)
+#0.1
+#Número real:
+precio = 7435.28
+print (precio)
+#7435.28
+#Booleano (verdadero / Falso):
+verdadero = True 
+falso = False
+
 
