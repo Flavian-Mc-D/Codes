245f23be61db4f62723dcf62808f59ce1791ed67
Flavian Mc Donald <flavian.mosquera@blueshift.com.br>
2019-02-17 11:04:56 -0300

    Arquivo sem 3

---
+++ Python.py
@@ -0,0 +1,17 @@
+# Comentarios de Python son iniciados con #
+#Python
+#Para ver información en pantalla:
+print('hola mundo')
+
+#Existen las Variables que son datos que pueden ser modificados, se representan con minusculas. 
+
+mi_variable = 5
+print (mi_variable)
+
+#Azul en S.T. es el color de print, fusía el = y morado el 5, en pantalla se vé el 5 solamente.
+
+#También existen las CONSTANTES que son valores fijos, no editables. representadas en mayusculas.
+
+MI_CONSTANTE = 'VALOR_NO_CAMBIANTE'
+print (MI_CONSTANTE)
+
